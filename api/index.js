0822d035c8af5b98239234b943e6f7c5ce2e8569
MXerFix <maksim2003003@gmail.com>
2023-12-25 17:30:21 +0300

    update prerelease

--- api/index.js
+++
@@ -1,3 +0,0 @@
-const app = require('../build/index')
-
-module.exports = app
--- controllers/token.controller.ts
+++ controllers/token.controller.ts
@@ -10,9 +10,9 @@ class TokenController {
       const { token } = req.body
       const user = await models.User.findOne({where: {token: token}})
       if (user) {
-        return res.json(true)
+        return res.json({status: true, user})
       } else {
-        return res.json(false)
+        return res.json({status: false, message: "Пользователь с таким идентификатором не найден!"})
       }
     } catch (error) {
       console.log(error)
