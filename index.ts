c54724560b5c7449d227badcf1072d9bea58847a
MXerFix <maksim2003003@gmail.com>
2023-12-25 06:13:42 +0300

    upd verc

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express"
 
 require('dotenv').config()
-import sequelize from './api/db'
+import sequelize from './db'
 import express from 'express'
-import models from './api/models/index'
+import models from './models/index'
 import fileUpload from 'express-fileupload'
 import path from 'path'
 import cors from 'cors'
-import router from './api/routes/index'
-import errorHandler from './api/middleware/errorHandle'
+import router from './routes/index'
+import errorHandler from './middleware/errorHandle'
 
 const IS_DEV = process.env.NODE_ENV === 'dev'
 const PORT = process.env.PORT
@@ -18,8 +18,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/static', express.static(path.resolve(import.meta.dir, './static')))
 app.use(fileUpload({}))
-app.use('/vote2024/api', router)
-app.get('/vote2024/api', (req: Request, res: Response) => {
+app.use('/api', router)
+app.get('/api', (req: Request, res: Response) => {
   return res.json({message: "api"})
 })
 
