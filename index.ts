bd1cc5bed3d30ddbee36637caba2bafae16b38e9
MXerFix <maksim2003003@gmail.com>
2023-12-25 18:14:53 +0300

    api/static

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use('/static', express.static(path.resolve(import.meta.dir, './static')))
+app.use('/api/static', express.static(path.resolve(import.meta.dir, './static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 app.get('/', (req: Request, res: Response) => {
